a12037c1a3f02467d1d28b593b3af7b534118423
Andrew Li <andrew159li@gmail.com>
2022-05-31 14:03:39 -0700

    testing exports in dockerrun

--- dockerrun.sh
+++ dockerrun.sh
@@ -3,8 +3,8 @@ cd ..
 # unzip apache-jena-4.5.0.tar.gz
 tar -xf apache-jena-4.5.0.tar.gz
 # add PATHS
-#export JENA_HOME='/app/apache-jena-4.5.0'
-#export PATH=$PATH:$JENA_HOME/bin
+export JENA_HOME='/app/apache-jena-4.5.0'
+export PATH=$PATH:$JENA_HOME/bin
 
 # do codeontology stuff here
 #cd parser
