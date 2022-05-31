245137dc0cbf1fddd162d8fbc596d3863622a53b
Andrew Li <andrew159li@gmail.com>
2022-05-31 13:43:28 -0700

    testing dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -3,12 +3,12 @@ cd ..
 # unzip apache-jena-4.5.0.tar.gz
 tar -xf apache-jena-4.5.0.tar.gz
 # add PATHS
-export JENA_HOME='/app/apache-jena-4.5.0'
-export PATH=$PATH:$JENA_HOME/bin
+#export JENA_HOME='/app/apache-jena-4.5.0'
+#export PATH=$PATH:$JENA_HOME/bin
 
 # do codeontology stuff here
-cd parser
-mvn package -DskipTests
+#cd parser
+#mvn package -DskipTests
 
 echo 'Test ApacheJena with sparql --version'
 echo 'Test Codeontology with ./codeontology --help'
